c9e51ab67d344cfdc051ef7f101bbb132526ed8c
QasemSe <q.serhi@gmail.com>
2022-07-04 15:16:19 +0300

    Lecture ^

---
+++ routes/admin.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/** route prefix */
+//Route::prefix('admin')->name('admin.')->group(function () {
+//    Route::get('/', function () { return "Admin Index Page"; });
+//    Route::get('posts', function () { return "Admin Posts"; })->name('admin.posts');
+//    Route::get('news', function () { return "Admin News"; })->name('admin.news');
+//    Route::get('comments', function () { return "Admin Comments"; })->name('admin.comments');
+//    Route::get('blog', function () { return "Admin Blog"; })->name('admin.blog');
+//});
+
+/** any route method show same result */
+//Route::match(['get', 'post'], '/welcome', function () {
+//    return 'Welcome User';
+//});
--- routes/web.php
+++ routes/web.php
@@ -13,16 +13,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('welcome/{name}', function (\Illuminate\Http\Request $request) {
-    return $request->name;
-});
+//Route::get('welcome/{name}', function (\Illuminate\Http\Request $request) {
+//    return $request->name;
+//});
 
 //
 //Route::get('news', function () {
 //    return "News";
 //});
 
-Route::get('news/{id?}', function ($id) {
-    return $id;
-});
+//Route::get('news/{id?}', function ($id) {
+//    return $id;
+//});
 
+
+/**
+ * Lec 6
+ */
+
+
+/** Route Name */
+//Route::get('/', function () {
+//    $user = "Ali";
+//    $post = 2;
+//    $comment = 3;
+//
+//    return route('comments', [$user, $post, $comment]);
+//});
+//
+//Route::get('contact-us', function () {
+//    return "Contact";
+//})->name('contactpage');
+//
+//Route::get('users/{user}/posts/{post}/comment/{comment}', function ($user, $post, $comment) {
+//    return "$user | $post | $comment";
+//})->name('comments');
